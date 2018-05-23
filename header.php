9c989f643cbc67dd0f24b57dfe7b280ffa9a6fe9
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-05-22 23:37:22 -0300

    Header structure updated

--- header.php
+++ header.php
@@ -15,68 +15,70 @@
     <link rel="stylesheet" href="_css/open-iconic.min.css">
 </head>
 <body>
-    <header>
-        <section class="header_general">
-            <div id="container_header" class="container-fluid bg-dark">
-                <div class="row  align-items-center">
-                    <div class="col-3 align-self-start">
-                        <h1 class="headertext">Phantasy Star Mania</h1>
-                    </div>
-                    <div class="col-3">
-                        <p class="headertext">We battle <br> for freedom!</p>
-                    </div>
-                    <div class="col-3">
-                        <p class="headertext">It's a battle <br>against the dark.</p>
-                    </div>
-                    <div class="col-3">
-                        <!-- área para contato e informações -->
+    <header class="bg-dark">
+        <div class="container">
+            <section class="header_general">
+                <div id="container_header">
+                    <div class="row  align-items-center">
+                        <div class="col-3 align-self-start">
+                            <h1 class="headertext">Phantasy Star Mania</h1>
+                        </div>
+                        <div class="col-3">
+                            <p class="headertext">We battle <br> for freedom!</p>
+                        </div>
+                        <div class="col-3">
+                            <p class="headertext">It's a battle <br>against the dark.</p>
+                        </div>
+                        <div class="col-3">
+                            <!-- área para contato e informações -->
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
             <!-- bar buttons -->
-        <nav class="navbar navbar-expand-lg nav-light bg-dark">
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
+            <nav class="navbar navbar-expand-lg nav-light">
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-auto">
 
-                    <li class="nav-item">
-                        <a href="characters.php" class="nav-link">
-                            <button type="submit" class="btn btn-primary">Personagens</button>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="characters.php" class="nav-link">
-                        <button type="submit" class="btn btn-primary">História</button>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="characters.php" class="nav-link">
-                        <button type="submit" class="btn btn-primary">Curiosidades</button>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="villains.php" class="nav-link">
-                            <button type="submit" class="btn btn-primary">Vilões</button>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="characters.php" class="nav-link">
-                            <button type="submit" class="btn btn-primary">Contato</button>
-                        </a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="characters.php" class="nav-link">
+                                <button type="submit" class="btn btn-primary">Personagens</button>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="characters.php" class="nav-link">
+                                <button type="submit" class="btn btn-primary">História</button>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="characters.php" class="nav-link">
+                                <button type="submit" class="btn btn-primary">Curiosidades</button>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="villains.php" class="nav-link">
+                                <button type="submit" class="btn btn-primary">Vilões</button>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="characters.php" class="nav-link">
+                                <button type="submit" class="btn btn-primary">Contato</button>
+                            </a>
+                        </li>
 
-                    <li class="nav-item dropdown">
-                        <!-- Menu dropdown -->
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Menu</a>
-                        <div class="dropdown-menu">
-                            <a href="index.php" class="dropdown-item">Página inicial</a>
-                            <a href="characters.php" class="dropdown-item">Personagens</a>
-                            <a href="villains.php" class="dropdown-item">Vilões</a>
-                            <a href="#" class="dropdown-item">História</a>
-                            <a href="#" class="dropdown-item">Curiosidades</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                        <li class="nav-item dropdown">
+                            <!-- Menu dropdown -->
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Menu</a>
+                            <div class="dropdown-menu">
+                                <a href="index.php" class="dropdown-item">Página inicial</a>
+                                <a href="characters.php" class="dropdown-item">Personagens</a>
+                                <a href="villains.php" class="dropdown-item">Vilões</a>
+                                <a href="#" class="dropdown-item">História</a>
+                                <a href="#" class="dropdown-item">Curiosidades</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </header>
