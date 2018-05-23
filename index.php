af41b499e264ee3efbf42de050258355184ad3c3
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-05-22 23:40:11 -0300

    Structure updated

--- index.php
+++ index.php
@@ -9,20 +9,23 @@ require_once('header.php');
 
 <main>
     <section id="start_container">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
                 <div class="col-12 col-md-6">
                     <h1 id="maintitle" class="mt-4 mb-4">Bem-vindo</h1>
 
-                    <p id="maintext"class="text-justify">
-                     Este é um site sobre Phantasy Star no geral. Apesar do conteúdo ser focado no quarto game da série, teremos artigos sobre todos os jogos da série. O site está em construção, por isso, pedimos que sejam pacientes. Qualquer crítica podem mandar para A SUA MÃE . ;)</br></br></p>
+                    <p class="maintext text-justify">
+                        Este é um site sobre Phantasy Star no geral. Apesar do conteúdo ser focado no quarto game da
+                        série, teremos artigos sobre todos os jogos da série. O site está em construção, por isso,
+                        pedimos que sejam pacientes. Qualquer crítica podem mandar para A SUA MÃE.;)</br></br></p>
 
-                    <p id="maintext" class="text-justify">Vivamus nec lobortis ligula, sit amet interdum sapien. Praesent ac libero
-                    nec sapien ornare hendrerit. Proin nec convallis massa, vel vulputate orci. Donec et diam ut
-                    orci blandit gravida. Mauris finibus justo ipsum, a consectetur lacus tempor vel. Etiam
-                    vulputate diam ac ante condimentum, eget facilisis tortor eleifend. Curabitur congue ante diam,
-                    ut pellentesque tortor luctus eu. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                    Vestibulum fringilla consectetur lacus vitae scelerisque. In nec semper eros.
+                    <p class="maintext text-justify">Vivamus nec lobortis ligula, sit amet interdum sapien. Praesent ac
+                        libero
+                        nec sapien ornare hendrerit. Proin nec convallis massa, vel vulputate orci. Donec et diam ut
+                        orci blandit gravida. Mauris finibus justo ipsum, a consectetur lacus tempor vel. Etiam
+                        vulputate diam ac ante condimentum, eget facilisis tortor eleifend. Curabitur congue ante diam,
+                        ut pellentesque tortor luctus eu. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                        Vestibulum fringilla consectetur lacus vitae scelerisque. In nec semper eros.
                     </p>
                 </div> <!-- deve ser responsivo -->
                 <div class="col-12 col-md-6">
