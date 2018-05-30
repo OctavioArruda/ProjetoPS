e63b5b743a013148de3559411704b15801b1e1b6
Schifer <oneiros241@gmail.com>
2018-05-30 12:18:12 -0300

    contact on index page landing page

--- index.php
+++ index.php
@@ -82,7 +82,24 @@ require_once('header.php');
 
     <section id="contact">
         <!-- contact section area with flexbox -->
-        
+        <div class="d-flex justify-content-center">
+            <div class="p2">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-dark" style="font-size:1.5em">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </section>
 </main>
 
